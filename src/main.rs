a1ba2a9eb6babb3840c015f37fa714959de72d97
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-27 13:01:50 -0800

    file content now read

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
+use std::fs;
 
 // Cikkect the command line arguments into a vector and print them
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     // Creating variables to hold the query argument and file path argument
     // Programs name takes up the first value [0] which is why we start the variables at [1]
     let query = &args[1];
@@ -11,4 +12,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{}", contents);
 }
