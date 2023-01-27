3836961774f347c4d31ba9eced2d4a876bf05d26
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-27 13:24:31 -0800

    extract parse_config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::env;
 use std::fs;
 
-// Cikkect the command line arguments into a vector and print them
+// Collect the command line arguments into a vector and print them
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // Creating variables to hold the query argument and file path argument
-    // Programs name takes up the first value [0] which is why we start the variables at [1]
-    let query = &args[1];
-    let file_path = &args[2];
+    
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -17,4 +15,14 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{}", contents);
+}
+
+// Creating variables to hold the query argument and file path argument
+// Programs name takes up the first value [0] which is why we start the variables at [1]
+// moved query and filepath logic to seperate function
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
