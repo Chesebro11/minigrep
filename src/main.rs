768457dc9fe57dfaa4b114d282d27b324f3b7480
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-26 16:28:54 -0800

    Accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+// Cikkect the command line arguments into a vector and print them
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    // Creating variables to hold the query argument and file path argument
+    // Programs name takes up the first value [0] which is why we start the variables at [1]
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
