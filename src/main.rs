607dedb5e4cfa6614bb4b370c7a9cbb3aa9fde53
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-31 10:58:11 -0800

    further error handling, commenting out uneeded function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 // Collect the command line arguments into a vector and print them
 fn main() {
@@ -14,18 +15,20 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-        run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 // This function is pulled out of main to help readability of the program
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-        println!("With text:\n{contents}");
+    println!("with text:\n{contents}");
+
+    Ok(())
 }
-
-//
 struct Config {
     query: String,
     file_path: String,
@@ -34,12 +37,12 @@ struct Config {
 // Return an isntance of a Config struct
 // using clone to fix possible ownership issues, however this takes more time and memory than storing a reference
 // This does make code straightforward not needing to manage lifetimes of the references
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
 
-    Config { query, file_path }
-}
+//     Config { query, file_path }
+// }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
