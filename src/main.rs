6e1abd13a6b10f4b1ebe074eb44f91a7c93516f1
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-31 10:38:06 -0800

    refactoring, error handling

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,50 @@
 use std::env;
 use std::fs;
+use std::process;
 
 // Collect the command line arguments into a vector and print them
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    
-    let (query, file_path) = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{}", contents);
 }
 
-// Creating variables to hold the query argument and file path argument
-// Programs name takes up the first value [0] which is why we start the variables at [1]
-// moved query and filepath logic to seperate function
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+//
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+// Return an isntance of a Config struct
+// using clone to fix possible ownership issues, however this takes more time and memory than storing a reference
+// This does make code straightforward not needing to manage lifetimes of the references
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
